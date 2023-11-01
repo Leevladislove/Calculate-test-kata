a7f98e41ca4fb897b92324f484657ecbeebed232
Lee Vlad <leevladislove@gmail.com>
2023-11-02 00:36:33 +0600

    correct func calculate/checkRange

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"strings"
 )
 
+
 func getInput() []string {
 	reader := bufio.NewReader(os.Stdin)
 	input, _ := reader.ReadString('\n')
@@ -15,8 +16,11 @@ func getInput() []string {
 }
 
 func checkRange(num1, num2 int) bool {
-	fmt.Println("Число должно быть в диапазоне от 1 до 10")
-	return num1 >= 1 && num1 <= 10 && num2 >= 1 && num2 <= 10
+	if (num1 < 1 || num1 > 10) || (num2 < 1 || num2 > 10) {
+		fmt.Println("Число должно быть в диапазоне от 1 до 10")
+		return false
+	}
+	return true
 }
 
 func checkCountArgument(arg []string) bool {
@@ -34,7 +38,7 @@ func calculate(expr []string) {
 
 	num1, err1 := strconv.Atoi(expr[0])
 	num2, err2 := strconv.Atoi(expr[2])
-	
+
 	if err1 != nil || err2 != nil {
 		fmt.Println("Неверные операнды")
 		return
@@ -53,10 +57,6 @@ func calculate(expr []string) {
 	case "*":
 		fmt.Println(num1 * num2)
 	case "/":
-		if num2 == 0 {
-			fmt.Println("Делить на 0 нельзя")
-			return
-		}
 		fmt.Println(num1 / num2)
 	default:
 		fmt.Println("Неверный оператор")
